6109c95a8806e3f203a15837378c99ca5aaeb011
miru6141 <miru6141@gmail.com>
2024-09-23 12:54:07 +0530

    update3

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin: 'https://assignmentmern121.netlify.app/',
+        origin: 'https://assignmentmern121.netlify.app',
     }
        
 ))
